0fb7e14069ed628e029942a1c8c020204e21d535
projectdanz <akunprojectdanz@gmail.com>
2025-11-30 06:50:16 +0700

    del: remove file for setup deploy in vercel

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Handle SQLite database creation for Vercel ephemeral environment
-$dbPath = '/tmp/database.sqlite';
-if (!file_exists($dbPath)) {
-    touch($dbPath);
-}
-
-require __DIR__ . '/../public/index.php';
